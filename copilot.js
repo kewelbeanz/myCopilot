9a693250bc2eba27a218f41a5125285f5a1a6122
Randal Hildebrand <randal.hildebrand@va.gov>
2021-07-14 15:52:41 -0400

    Update copilot.js

--- copilot.js
+++ copilot.js
@@ -1,4 +1,8 @@
 /* Use async and await to wait for page to load */
 async function myFunction(){
-    
+    let myPromise=new Promise(function(myResolve,myReject){
+        myResolve(location.href,"https://logicole.health.mil");
+    });
+    document.getElementById("okButton").click();
+    document.getElementById("loginButton").click();
 }
